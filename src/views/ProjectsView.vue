a7f9bf49474afeb1db813757c2ae5c6fa336880c
ChrisanerM <chrisanermatthys04@gmail.com>
2023-01-14 14:23:40 +0200

    projects

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,6 +1,6 @@
 <template>
-  <h2>Testimonials</h2>
-<dim v class="container" id="link" style="display:inline-block;">
+  <h2>Projects</h2>
+<dim v class="container" id="netlify" style="display:inline-block;">
 <div class="row justify-content-center cols-3">
  <div class="flip-card" style="background-color: tan;border:solid 4px;border-radius:12px; border-color:brown;" v-for="item in testimonials">
     <div class="flip-card-inner">
@@ -9,11 +9,12 @@
          :src="item.img"
           alt="Avatar"
           style="width: 100%; height: 100%;"/>
+          <a :href="item.netlify">Netlify</a>
+          <a :href="item.github">Github</a>
       </div>
       <div class="flip-card-back">
         <h1>{{item.description}}</h1>
-        <h4>{{item.date}}</h4>
-        <p>{{item.link}}</p>
+        <p>{{item.netlify}}</p>
        </div>
     </div>
   </div>
@@ -22,40 +23,40 @@
 </template>
 <script>
 export default {
-testimonials: "link-cards",
+testimonials: "netlify-cards",
 data(){
     return{
         testimonials:[
-            {description:'Megan Alexander',
-            date:'Ex-colleague',
-            img:'https://i.postimg.cc/bvh6D3hM/alexander-jpg.jpg',
-            link:'Chrisaner is a very quiet person but very friendly, dedicated and hard worker. '
+            {description:'HTML and CSS practise',
+            img:'https://i.postimg.cc/3rp3xyst/htmlandcssprac.jpg',
+            netlify:'https://htmlcssprac.netlify.app/',
+            github: '',
             },
-            {description:'Kauthar Dreyer',
-            date:'Ex-colleague',
-            img:'https://i.postimg.cc/3RbCBZs3/dreyer.jpg',
-            link: "She's a kind, loving and friendly individual whom you may always ask help for."
-            },
-            {description:'Abigail Temm',
-            date:'Ex-colleague',
-            img:'https://i.postimg.cc/T10y02Lg/temm-jpg.jpg',
-            link:'Always prepared for the day and ensures all tasks are completed to the best of her abiliy.'
-            },
-            {description:'Beyonce Leonards',
-            date:'Colleague',
-            img:'https://i.postimg.cc/kGJnx0f4/Beyonce-Leonards.jpg',
-            link:' A well mannered individual, always up for new challenges. She sets out goals & works hard to achieve them.'
-            },
-            {description:'Zoe Daniels',
-            date:'Colleague',
-            img:'https://i.postimg.cc/447DKFWQ/Zoe-Daniels.jpg',
-            link:' Its just amazing to work with such a kind personality. I highly recommend Chrisaner Matthys  for any position or endeavor that she may pursue. She would be a valueable asset to your company'
-            },
-            {description:'Xolelwa Manqina',
-            date:'Colleague',
-            img:'https://i.postimg.cc/GhF4d9RG/Xolelwa-Manqina.jpg',
-            link:'Chrisaner is a hard-working and strong individual that ensures she understands something completely before implementing it. She has the ability to assess an issue and solve it, she strives to perfection in her work.',
-            }
+            {description:'Bootstrap EOMP',
+            img:'https://i.postimg.cc/tgggsSXy/EOMP1.png',
+            netlify: "https://chrisaner-portfolio.netlify.app/",
+            github: '',
+          },
+            {description:'E-Commerce',
+            img:'https://i.postimg.cc/8P4Q7Ltm/E-commerce.jpg',
+            netlify:'',
+            github: '',
+          },
+            {description:'Calculator JS',
+            img:'https://i.postimg.cc/nLZD3bvJ/cal-js.jpg',
+            netlify:'https://chrisanermatthys-calculator.netlify.app/',
+            github: '',
+          },
+            {description:'JS_EOMP',
+            img:'https://i.postimg.cc/ry7gg3bn/JS-EOMP.png',
+            netlify:' https://finaljs-cleanexpress.netlify.app/',
+            github: '',
+          },
+            {description:'My First Vue App',
+            img:'https://i.postimg.cc/W3kq4Wnz/First-Vue-App.png',
+            netlify:'',
+            github: '',  
+          }
         ]
     }
 }
